2f0fd566199bef128f480bf5e4c704d5e62e07b3
SaurabhSonde <saurabhsonde111@gmail.com>
2021-09-13 15:37:26 +0530

    added middlewares

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const youtubeRoutes = require("./routes/upload");
 
 const app = express();
-
+// middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+app.use(express.json({ extented: false }));
 // routes
-app.use("/api", youtubeRoutes);
+app.use("/", youtubeRoutes);
 
 app.listen(5000, () => {
   console.log("Server in running on port 5000🚀");
