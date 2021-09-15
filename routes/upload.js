ce22b807a5a737a7d144cc181e503237f3cc0287
SaurabhSonde <saurabhsonde111@gmail.com>
2021-09-15 19:10:43 +0530

    added routes for video brodcast

--- routes/upload.js
+++ routes/upload.js
@@ -4,11 +4,13 @@ const {
   callback,
   uploadVideo,
   upload,
+  broadcastVideo,
 } = require("../controllers/upload");
 const router = express.Router();
 
 router.get("/", authenticate);
 router.get("/google/callback", callback);
 router.post("/upload", upload, uploadVideo);
+router.post("/broadcast", upload, broadcastVideo);
 
 module.exports = router;
