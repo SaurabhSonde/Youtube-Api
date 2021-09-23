dd6d912ddf1b16a60727d50da0bd17ee664b9e01
SaurabhSonde <saurabhsonde111@gmail.com>
2021-09-23 14:10:55 +0530

    added route for thumbnail

--- routes/upload.js
+++ routes/upload.js
@@ -5,12 +5,14 @@ const {
   uploadVideo,
   upload,
   broadcastVideo,
+  uploadThumbnail,
 } = require("../controllers/upload");
 const router = express.Router();
 
 router.get("/", authenticate);
 router.get("/google/callback", callback);
 router.post("/upload", upload, uploadVideo);
+router.post("/upload/thumbnail", upload, uploadThumbnail);
 router.post("/broadcast", upload, broadcastVideo);
 
 module.exports = router;
